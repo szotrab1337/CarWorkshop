da7eddde05072a0d49dc7d23132e471ef427fd2f
szotrab1337 <tazzaq123@gmail.com>
2023-08-30 20:46:33 +0200

    Add project files.

---
+++ CarWorkshop.MVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using CarWorkshop.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CarWorkshop.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult About()
+        {
+            var vm = new AboutViewModel
+            {
+                Title = "About",
+                Description = "Description",
+                Tags = new[] { "Tag1", "Tag2" }
+            };
+
+            return View(vm);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CarWorkshop.MVC/Models/AboutViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CarWorkshop.MVC.Models
+{
+    public class AboutViewModel
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string[] Tags { get; set; } = null!;
+    }
+}
