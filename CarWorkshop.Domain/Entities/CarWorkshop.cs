cc40e7dd52ce9d99da3c59ef1aba156d301dcb8a
szotrab1337 <tazzaq123@gmail.com>
2023-08-31 21:13:50 +0200

    Added entity framework and data seeder.

--- CarWorkshop.Domain/Entities/CarWorkshop.cs
+++ CarWorkshop.Domain/Entities/CarWorkshop.cs
@@ -4,12 +4,12 @@ namespace CarWorkshop.Domain.Entities
 {
     public class CarWorkshop
     {
-        [Required]
         public int Id { get; set; }
         public string Name { get; set; } = default!;
         public string? Description { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public CarWorkshopContactDetails ContactDetails { get; set; } = default!;
+        public string? About { get; set; }
 
         public string EncodedName { get; private set; } = default!;
 
---
+++ CarWorkshop.Infrastructure/Seeders/CarWorkshopSeeder.cs
@@ -0,0 +1,37 @@
+﻿using CarWorkshop.Infrastructure.Persistence;
+
+namespace CarWorkshop.Infrastructure.Seeders
+{
+    public class CarWorkshopSeeder
+    {
+        private readonly CarWorkshopDbContext _dbContext;
+
+        public CarWorkshopSeeder(CarWorkshopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Seed()
+        {
+            if (await _dbContext.Database.CanConnectAsync() && !_dbContext.CarWorkshops.Any())
+            {
+                var mazdaAso = new Domain.Entities.CarWorkshop()
+                {
+                    Name = "Mazda ASO",
+                    Description = "Autoryzowany serwis Mazda",
+                    ContactDetails = new()
+                    {
+                        City = "Kraków",
+                        Street = "Szewska 2",
+                        PostalCode = "30-001",
+                        PhoneNumber = "+48699222888"
+                    }
+                };
+                mazdaAso.EncodeName();
+
+                await _dbContext.CarWorkshops.AddAsync(mazdaAso);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
