a46d34468d26cab6e5fe323d8ad81b614fd27c23
szotrab1337 <tazzaq123@gmail.com>
2023-09-04 20:19:58 +0200

    Added user identity.

--- CarWorkshop.Infrastructure/Extensions/ServiceCollectionExtension.cs
+++ CarWorkshop.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -2,6 +2,7 @@
 using CarWorkshop.Infrastructure.Persistence;
 using CarWorkshop.Infrastructure.Repositories;
 using CarWorkshop.Infrastructure.Seeders;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,6 +15,9 @@ namespace CarWorkshop.Infrastructure.Extensions
         {
             services.AddDbContext<CarWorkshopDbContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("CarWorkshop")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<CarWorkshopDbContext>();
 
             services.AddScoped<CarWorkshopSeeder>();
             services.AddScoped<ICarWorkshopRepository, CarWorkshopRepository>();
